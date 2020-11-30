78687e6bcf56ea225c79b6265ba96fbf3f210c8b
Matias <gotzmatiascode@gmail.com>
2020-11-30 15:15:54 -0300

    Basic Window
    Made a BAsic window to change it to preferences, it need to be connected to the menu bar item

--- macos/spotter-macOS/AppDelegate.swift
+++ macos/spotter-macOS/AppDelegate.swift
@@ -19,16 +19,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "spotter", initialProperties: nil, launchOptions: nil)
     let rootViewController = NSViewController()
     rootViewController.view = rootView
+    //Menu Bar Item
     let statusBar = NSStatusBar.system
         statusBarItem = statusBar.statusItem(
             withLength: NSStatusItem.squareLength)
-        statusBarItem.button?.title = "☂︎"
+        statusBarItem.button?.title = "Find"//Should change to spotter logo in future
         let statusBarMenu = NSMenu(title: "Cap Status Bar Menu")
         statusBarItem.menu = statusBarMenu
         statusBarMenu.addItem(
             withTitle: "Open Spotter",
             action: #selector(AppDelegate.openspotter),
-            keyEquivalent: "k")
+            keyEquivalent: "")
         statusBarMenu.addItem(
             withTitle: "Preferences",
             action: #selector(AppDelegate.openpreferences),
@@ -37,16 +38,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             withTitle: "Quit",
             action: #selector(AppDelegate.Quitapp),
             keyEquivalent: "q")
+    //End Menu Bar Item
   }
-
+  //Menu bar functions
    @objc func openspotter() {
-        print("open")
+        print("open")//This should open spotter
     }
     @objc func openpreferences() {
-        print("Preferences")
+        print("Preferences")//This should open the preferences window
     }
     @objc func Quitapp() {
-        NSApp.terminate(self)
+        NSApp.terminate(self)//Quit the app
     }
   }
 
