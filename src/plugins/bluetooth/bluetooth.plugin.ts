3be26f1617163c35595e8ed2a87e7b6c3a10f221
Denis Zyulev <zzulev@gmail.com>
2021-01-06 22:12:12 +0100

    Implement bluetooth connection func

--- src/plugins/bluetooth/bluetooth.plugin.ts
+++ src/plugins/bluetooth/bluetooth.plugin.ts
@@ -1,4 +1,4 @@
-import { SpotterOption, SpotterPlugin, SpotterPluginLifecycle, spotterSearch } from '../../core';
+import { SpotterBluetoothItem, SpotterOption, SpotterPlugin, SpotterPluginLifecycle, spotterSearch } from '../../core';
 import icon from './icon.png';
 
 export class BluetoothPlugin extends SpotterPlugin implements SpotterPluginLifecycle {
@@ -14,12 +14,46 @@ export class BluetoothPlugin extends SpotterPlugin implements SpotterPluginLifec
         title: device.name,
         subtitle: device.connected ? 'Connected' : 'Disconnected',
         action: () => device.connected
-          ? this.nativeModules.bluetooth.disconnectDevice(device.address)
-          : this.nativeModules.bluetooth.connectDevice(device.address),
+          ? this.disconnectDevice(device.address)
+          : this.connectDevice(device.address),
         image: icon,
       })),
       'bluetooth',
     );
+  }
+
+  private connectDevice(address: string): Promise<boolean> {
+    this.nativeModules.bluetooth.connectDevice(address);
+    return this.executeCommandWithTimer(
+      devices => devices.find(d => d.address === address)?.connected,
+    );
+  }
+
+  private disconnectDevice(address: string) {
+    this.nativeModules.bluetooth.disconnectDevice(address);
+    return this.executeCommandWithTimer(
+      devices => !devices.find(d => d.address === address)?.connected,
+    );
+  }
+
+  private executeCommandWithTimer(
+    check: (devices: SpotterBluetoothItem[]) => boolean | undefined,
+  ): Promise<boolean> {
+    return new Promise(res => {
+      let seconds = 0;
+      const maxSeconds = 5;
+      const interval = setInterval(async () => {
+        seconds++;
+        const connectedDevices = await this.nativeModules.bluetooth.getDevices();
+        if (check(connectedDevices)) {
+          clearInterval(interval);
+          res(true);
+        } else if (seconds >= maxSeconds) {
+          clearInterval(interval);
+          res(false);
+        }
+      }, 1000);
+    });
   }
 
 }
