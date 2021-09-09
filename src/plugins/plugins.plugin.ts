ecfd13c35a413b37b075f7b0150ac1233ccafa6b
Denis Z <zyulev@icloud.com>
2021-09-09 10:13:01 +0200

    Add install plugin autocomplete

--- src/plugins/plugins.plugin.ts
+++ src/plugins/plugins.plugin.ts
@@ -1,6 +1,7 @@
 import { InternalPlugin, InternalPluginLifecycle, InternalPluginOption } from "../core";
 import icon from '../../preview/icon.png';
 import { Settings } from "../providers/settings.provider";
+import fetch from "node-fetch";
 
 const LIBRARY = [
   'spotter-applications-plugin',
@@ -18,32 +19,56 @@ export class PluginsPlugin extends InternalPlugin implements InternalPluginLifec
 
         const settings: Settings = await this.getSettings();
 
-        const queryPackages = await this.api.shell.execute(`
-          npm search ${query} --json
-        `);
-
-        console.log(queryPackages);
-
         return [
           {
-            title: 'Install ...',
+            title: 'Install',
             icon,
-            queryAction: (query: string) => ([
-              ...LIBRARY
-                .filter(plugin => !settings.plugins.find(p => p === plugin))
-                .map(plugin => ({
-                  title: plugin,
+            queryAction: async (query: string) => {
+              if (!query?.length) {
+                return [];
+              }
+
+              const queryPackages = await fetch(
+                `https://www.npmjs.com/search/suggestions?q=${query}`
+              ).then(r => r.json());
+
+              if (queryPackages) {
+                return queryPackages.map((p: any) => ({
+                  title: p.name,
                   action: async () => {
                     try {
+                      const testCommand = {
+                        type: 'onInit',
+                        storage: {},
+                      }
                       // TODO: add installing spinner
-                      await this.api.shell.execute(`npm i -g ${plugin}`);
-                      this.registerPlugin(plugin);
-                    } catch {
+                      await this.api.shell.execute(`npx ${p.name} ${JSON.stringify(testCommand)}`);
 
+                      await this.api.shell.execute(`npm i -g ${p.name}`);
+                      this.registerPlugin(p.name);
+                    } catch {
+                      console.log('INSTALLATION ERROR');
                     }
-                  },
-                }))
-            ]),
+                  }
+                }));
+              }
+            }
+            // queryAction: (query: string) => ([
+            //   ...LIBRARY
+            //     .filter(plugin => !settings.plugins.find(p => p === plugin))
+            //     .map(plugin => ({
+            //       title: plugin,
+            //       action: async () => {
+            //         try {
+            //           // TODO: add installing spinner
+            //           await this.api.shell.execute(`npm i -g ${plugin}`);
+            //           this.registerPlugin(plugin);
+            //         } catch {
+
+            //         }
+            //       },
+            //     }))
+            // ]),
           },
           ...(settings.plugins.map(plugin => ({
             title: plugin,
