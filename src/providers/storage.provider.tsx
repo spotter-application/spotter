0872a62730d57f6d172f4fb08947fce19221ae9a
Denis Z <ziulev@pm.me>
2021-11-28 18:46:14 +0100

    Pass tokens with storage

--- src/providers/storage.provider.tsx
+++ src/providers/storage.provider.tsx
@@ -1,12 +1,25 @@
 import { Storage } from '@spotter-app/core';
 import React, { FC, useRef } from 'react';
 import { useApi } from './api.provider';
+import {
+ SPOTIFY_CLIENT_ID,
+ SPOTIFY_REDIRECT_URI,
+ SPOTIFY_CLIENT_SECRET,
+} from '@env';
 
 const PLUGINS_STORAGE_KEY = 'PLUGINS_STORAGE';
 
 type Context = {
-  getStorage: () => Promise<Storage>;
+  getStorage: (plugin?: string) => Promise<Storage>;
   patchStorage: (data: Storage) => void;
+};
+
+const tokens = {
+  spotify: {
+    clientId: SPOTIFY_CLIENT_ID,
+    clientSecret: SPOTIFY_CLIENT_SECRET,
+    redirectUri: SPOTIFY_REDIRECT_URI,
+  },
 };
 
 const context: Context = {
@@ -22,18 +35,30 @@ export const StorageProvider: FC<{}> = (props) => {
 
   const cachedStorage = useRef<Storage>();
 
-  const getStorage = async () => {
+  const getStorage = async (plugin?: string) => {
     if (cachedStorage.current) {
-      return cachedStorage.current;
+      return {
+        ...(plugin
+          ? (cachedStorage.current[plugin] ?? {})
+          : cachedStorage.current
+        ),
+        tokens,
+      };
     }
 
     const storage = await api.storage.getItem<Storage>(PLUGINS_STORAGE_KEY);
     if (!storage) {
-      return {};
+      return {tokens};
     }
 
     cachedStorage.current = storage;
-    return storage;
+    return {
+      ...(plugin
+        ? (storage[plugin] ?? {})
+        : storage
+      ),
+      tokens,
+    };
   }
 
   const patchStorage = async (data: Storage) => {
