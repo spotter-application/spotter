368fbb8445fbaa1bc7bc8bc3cb77a4ca00029ddb
Denis Z <zyulev@icloud.com>
2021-12-02 00:47:06 +0100

    Immplement WS

---
+++ macos/spotter-macOS/XCallbackUrl/XCallbackUrl.m
@@ -0,0 +1,8 @@
+//
+//  XCallbackUrl.m
+//  spotter-macOS
+//
+//  Created by Denis on 30.11.21.
+//
+
+#import <Foundation/Foundation.h>
---
+++ macos/spotter-macOS/XCallbackUrl/XCallbackUrl.swift
@@ -0,0 +1,8 @@
+//
+//  XCallbackUrl.swift
+//  spotter-macOS
+//
+//  Created by Denis on 30.11.21.
+//
+
+import Foundation
