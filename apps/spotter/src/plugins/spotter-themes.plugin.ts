054b51e031e61700434ad1177d2300118f33ae02
Denis Z <ziulev@pm.me>
2021-12-11 23:07:19 +0100

    Add github dimmed theme

--- apps/spotter/src/plugins/spotter-themes.plugin.ts
+++ apps/spotter/src/plugins/spotter-themes.plugin.ts
@@ -3,13 +3,20 @@ import { SpotterPlugin } from "@spotter-app/core";
 export class SpotterThemesPlugin extends SpotterPlugin {
 
   async onInit() {
-    this.spotter.registries.options.set([{
+    this.spotter.setRegisteredOptions([{
       title: 'Spotter themes',
       onQuery: () => ([
         {
           title: 'Dark theme (active)',
           onHover: () => {
             this.spotter.setTheme('#212121,#ffffff,#3c3c3c,#ffffff,#0f60cf,#fefefe');
+            return false;
+          },
+        },
+        {
+          title: 'Github dimmed theme',
+          onHover: () => {
+            this.spotter.setTheme('#1c2128,#adbac7,#2d333b,#ffffff,#0f60cf,#fefefe');
             return false;
           },
         },
