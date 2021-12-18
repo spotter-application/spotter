90648398a36fd90054e82b7dc64cd01f3634b402
Denis Z <ziulev@pm.me>
2021-12-18 14:13:45 +0100

    Add root folder option

--- packages/vscode-plugin/src/vscode-plugin.ts
+++ packages/vscode-plugin/src/vscode-plugin.ts
@@ -1,6 +1,10 @@
 import { OnQueryOption } from '@spotter-app/core';
-import { Plugin, promisedExec } from '@spotter-app/plugin';
+import { onQueryFilter, Plugin, promisedExec } from '@spotter-app/plugin';
 import packageJSON from '../package.json';
+
+interface Storage {
+  folders: string[],
+}
 
 new class CalculatorPlugin extends Plugin {
   private appPath = '/Applications/Visual Studio Code.app';
@@ -20,29 +24,117 @@ new class CalculatorPlugin extends Plugin {
       icon: this.appPath,
       replaceOptions: ['Visual Studio Code'],
       onSubmit: this.open,
-      onQuery: async (q: string) => await this.getFolders(q),
+      onQuery: async (q: string) => await this.getOptions(q),
     }]);
   }
 
-  private async getFolders(q: string) {
-    const root = await promisedExec(`echo $HOME`).then(r => r.split('\n')[0]);
-    const folders: OnQueryOption[] = await promisedExec('ls ~/Developer')
-      .then(ls => ls
-        .split('\n')
-        .filter(folder => !!folder)
-        .map(folder => ({
-          title: folder,
-          icon: this.appPath,
-          onSubmit: () => this.openProject(`${root}/Developer/${folder}`),
-        }))
-      )
-      .catch(() => []);
+  private async getOptions(q: string): Promise<OnQueryOption[]> {
+    // const projects = await this.getDeepGitProjects('~/Developer');
+    const storage = await this.spotter.getStorage<Storage>();
+    const projects = await (storage.folders ?? [])
+      .reduce<Promise<OnQueryOption[]>>(async (acc, f) => {
+        const resolvedAcc = await acc;
+        const projects = await this.getDeepGitProjects(f);
+        return [
+          ...resolvedAcc,
+          ...projects,
+        ];
+      }, Promise.resolve<OnQueryOption[]>([]))
 
-    if (!q.length) {
-      return folders;
+    return onQueryFilter(
+      q,
+      [
+        {
+          title: 'Add root folder',
+          onQuery: q => this.getFolders(q),
+          icon: this.appPath,
+        },
+        ...projects,
+      ],
+    );
+  }
+
+  private async addRootFolder(folder: string) {
+    const storage = await this.spotter.getStorage<Storage>();
+    this.spotter.setStorage<Storage>({
+      folders: [
+        ...(storage.folders ?? []),
+        folder,
+      ],
+    });
+  }
+
+  private async getFolders(query: string) {
+    this.spotter.setPlaceholder('New root folder path...');
+    if (!query) {
+      return [];
     }
 
-    return folders.filter(f => f.title.startsWith(q));
+    let folder = '';
+
+    if (query.endsWith('/')) {
+      folder = query.substring(0, query.length - 1);
+    } else {
+      const paths = query.split('/');
+      paths.pop();
+      folder = paths.join('/');
+    }
+
+    const folders: OnQueryOption[] = await promisedExec(`cd ${folder} && ls`)
+      .then(ls => ls
+        .split('\n')
+        .filter(f => !!f)
+        .reduce<Promise<OnQueryOption[]>>(async (acc, f) => {
+          const resolvedAcc = await acc;
+          return [
+            ...resolvedAcc,
+            {
+              title: `${folder}/${f}`,
+              subtitle: 'Add as a root folder',
+              icon: this.appPath,
+              onSubmit: () => this.addRootFolder(`${folder}/${f}`),
+            },
+          ];
+        }, Promise.resolve<OnQueryOption[]>([]))
+      )
+      .catch(() => []);
+    
+    return onQueryFilter(query, folders);
+  }
+
+  private async getDeepGitProjects(folder: string) {
+    const folders: OnQueryOption[] = await promisedExec(`cd ${folder} && ls`)
+      .then(ls => ls
+        .split('\n')
+        .filter(f => !!f)
+        .reduce<Promise<OnQueryOption[]>>(async (acc, f) => {
+          const resolvedAcc = await acc;
+
+          const hasGit = await promisedExec(`cd ${folder}/${f} && ls -all`)
+            .then(r => r.includes('.git'))
+            .catch(() => false);
+
+          if (!hasGit) {
+            const childProjects = await this.getDeepGitProjects(`${folder}/${f}`)
+            return [
+              ...childProjects,
+              ...resolvedAcc,
+            ];
+          }
+
+          return [
+            ...resolvedAcc,
+            {
+              title: `${folder}/${f}`,
+              icon: this.appPath,
+              onSubmit: () => this.openProject(`${folder}/${f}`),
+            },
+          ];
+        }, Promise.resolve<OnQueryOption[]>([]))
+      )
+      .catch(() => []);
+    
+    return folders;
   }
 
   private open() {
