9b0facf14a59d1414b52adebdf2ae21089888c5d
Denis Ziulev <ziulev@pm.me>
2022-11-16 22:39:28 +0100

    Macos layout

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,12 +2,27 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
+  let width = 600
+  let height = 500
+  
   override func awakeFromNib() {
     let flutterViewController = FlutterViewController.init()
     let windowFrame = self.frame
     self.contentViewController = flutterViewController
     self.setFrame(windowFrame, display: true)
-
+    self.styleMask = [
+      .nonactivatingPanel,
+      .closable,
+      .titled,
+      .resizable,
+    ]
+    self.makeKeyAndOrderFront(nil)
+    self.isMovableByWindowBackground = false
+    self.styleMask.remove(.titled)
+    self.level = .mainMenu
+    self.isOpaque = false
+    self.backgroundColor = NSColor.clear
+    
     RegisterGeneratedPlugins(registry: flutterViewController)
 
     super.awakeFromNib()
